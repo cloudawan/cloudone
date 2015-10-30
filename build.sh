ef97188ef035317a2173818ff314d469de0b2a1f
edcyc <edcyc.tw@gmail.com>
2015-10-30 21:41:04 +0800

    Add install golang

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+sudo apt-get install -y golang
 go build
 mv kubernetes_management docker/kubernetes_management/
 find ! -wholename './docker/*' ! -wholename './docker' ! -wholename '.' -exec rm -rf {} +
