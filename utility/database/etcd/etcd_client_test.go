60611edb93213b3eb2e0bc9acaef8d9cf66df945
edcyc <edcyc.tw@gmail.com>
2016-03-11 02:39:21 -0600

    Test case

---
+++ utility/database/etcd/etcd_client_test.go
@@ -0,0 +1,38 @@
+// Copyright 2015 CloudAwan LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package etcd
+
+/*
+import (
+	"fmt"
+	"github.com/coreos/etcd/client"
+	"golang.org/x/net/context"
+	"testing"
+)
+
+
+func TestCreateDirectoryIfNotExist(t *testing.T) {
+	fmt.Println(EtcdClient.CreateDirectoryIfNotExist("aaa"))
+}
+
+func TestDeleteDirectory(t *testing.T) {
+	keysAPI, err := EtcdClient.GetKeysAPI()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(keysAPI.Delete(context.Background(), "aaa", &client.DeleteOptions{Dir: true}))
+	}
+}
+*/
